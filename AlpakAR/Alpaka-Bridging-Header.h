3078e62cbd96fdb3454c1b37302c4d2cd736e4b7
Marvin von Hagen <M@vHagen.me>
2017-09-10 00:55:40 +0200

    60fps 00:55

---
+++ AlpakAR/Alpaka-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Alpaka-Bridging-Header.h
+//  
+//
+//  Created by Felix Schlegel on 09.09.17.
+//
+//
+
+#ifndef Alpaka_Bridging_Header_h
+#define Alpaka_Bridging_Header_h
+
+#import <TesseractOCR/TesseractOCR.h>
+
+#endif /* Alpaka_Bridging_Header_h */
